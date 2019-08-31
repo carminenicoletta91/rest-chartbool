3a1d9eaccdb44b1f4f76c4e5a0f8cd0a66f88c2a
carminenicoletta91 <carminenicolettakr91@gmail.com>
2019-08-31 16:09:18 +0200

    --m3 completata--

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ function init() {
     $(".input button").click(getvalueforPost);
     getammountfordate();
     getamountforsalesname();
+    getgraphbar();
 }
 
 $(document).ready(init);
-
+function venditeforquarter(data){
+  console.log(data);
+}
 function getmonthlist(){
   var month= ["gennaio",
               "febbraio",
@@ -21,7 +24,7 @@ function getmonthlist(){
               "ottobre",
               "novembre",
               "dicembre"];
-  console.log(month);
+  // console.log(month);
   return month;
 }
 function getname(data){
@@ -29,9 +32,9 @@ function getname(data){
   var namedata;
   for(var i=0;i<data.length;i++){
     if(namelist.includes(data[i].salesman)){
-      console.log("è incluso ");
+      // console.log("è incluso ");
     }else{
-    console.log(data[i].salesman,"i:",i);
+    // console.log(data[i].salesman,"i:",i);
     namelist.push(data[i].salesman);
     }
     // console.log(namelist);
@@ -62,7 +65,7 @@ function getammount(data){
 
   }
 
-  console.log(monthammount);
+  // console.log(monthammount);
   return monthammount;
 }
 function getammountforname(data){
@@ -81,7 +84,7 @@ function getammountforname(data){
       // console.log(data[i].salesman);
       // console.log(dipendenti[j].name,"nome");
       if(data[i].salesman==dipendenti.name[j]){
-        console.log("sei qui");
+        // console.log("sei qui");
         dipendenti.ammontare[j]+=Number(data[i].amount);
       }
     }
@@ -91,8 +94,8 @@ function getammountforname(data){
 }
 function readamount(obj,amounttotal){
   var amount=[];
-  console.log(obj.name);
-  console.log(obj.ammontare);
+  // console.log(obj.name);
+  // console.log(obj.ammontare);
   for(var i=0;i<obj.name.length;i++){
     amount.push(((obj.ammontare[i]/amounttotal)*100).toFixed(2));
 
@@ -172,11 +175,11 @@ function getvalueforPost(){
 
   var monthvalue=$(".select-month select#month").val();
   var monthString="01/"+monthvalue+"/2017";
-  console.log(monthvalue);
+  // console.log(monthvalue);
   var amountinput=$(".input input").val();
-  console.log(amountinput);
+  // console.log(amountinput);
   var parseamount=Number(amountinput);
-  console.log("name:",namevalue,"month:",monthvalue,"amount:",parseamount);
+  // console.log("name:",namevalue,"month:",monthvalue,"amount:",parseamount);
 
   $.ajax({
     url:"http://157.230.17.132:4012/sales",
@@ -187,7 +190,7 @@ function getvalueforPost(){
       date:monthString,
     },
     success:function(data){
-      console.log(data);
+      // console.log(data);
       $(".select-salesman select#salesman").val("reset");
       $(".select-month select#month").val("reset");
       $(".input input").val("");
@@ -195,6 +198,92 @@ function getvalueforPost(){
     },
     error:function(error){
       alert("error");
+    },
+  });
+}
+function getSalesNumberForQuarter(data){
+  var quarterobj=new Object;
+  quarterobj.amountq1=0;
+  quarterobj.amountq2=0;
+  quarterobj.amountq3=0;
+  quarterobj.amountq4=0;
+  var monthnumber;
+  var monthparse;
+  for (var i=0;i<data.length;i++){
+    monthnumber = data[i].date;
+    // console.log(monthnumber);
+    monthparse =moment(monthnumber,"DD/MM/YYYY").month();
+    // console.log(monthparse);
+    switch (monthparse) {
+      //mesi da gennaio a marzo q1
+      case 0:
+      case 1:
+      case 2:
+        quarterobj.amountq1+=1;
+      break;
+      //mesi da aprile a giugno q2
+      case 3:
+      case 4:
+      case 5:
+        quarterobj.amountq2+=1;
+      break;
+      //mesi da luglio a settembre q3
+      case 6:
+      case 7:
+      case 8:
+        quarterobj.amountq3+=1;
+      break;
+      //mesi da ottobre a dicembre q4
+      case 9:
+      case 10:
+      case 11:
+        quarterobj.amountq4+=1;
+      break;
+    }
+  }
+  console.log(quarterobj);
+  return quarterobj;
+}
+function getgraphbar(){
+  $.ajax({
+    url:"http://157.230.17.132:4012/sales",
+    method:"GET",
+    success:function(data){
+      var objnumerovendite=getSalesNumberForQuarter(data);
+      console.log(objnumerovendite,"oggetto in success");
+      var q1=Number(objnumerovendite.amountq1);
+      var q2=Number(objnumerovendite.amountq2);
+      var q3=Number(objnumerovendite.amountq3);
+      var q4=Number(objnumerovendite.amountq4);
+      console.log("q1:",q1,"q2:",q2,"q3:",q3,"q4",q4);
+      var ctx = document.getElementById('myChart-bar').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type:'bar',
+        data: {
+        labels: ["q1","q2","q3","q4"],
+        datasets: [{
+            label: 'Vendite for quarter',
+            data: [q1,q2,q3,q4],
+            backgroundColor:"rgba(0,139,139,0.5)",
+            borderColor: 'rgba(0,0,139,1)',
+            borderWidth: 1
+        }]
+      },
+        options: {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            },
+          },
+
+      });
+    },
+    error:function(){
+      alert("errore");
     },
   });
 }
